9232f5b6c5e293faec60a5491ae6f194a49ed293
jose <jchatatajallo@gmail.com>
2023-10-23 16:47:30 -0500

    cambios Jose

---
+++ src/main/java/com/app/controller/TiendaController.java
@@ -0,0 +1,56 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.app.modelos.Marca;
+import com.app.modelos.Producto;
+import com.app.repository.IMarcaRepository;
+import com.app.repository.IProductoRepository;
+
+@Controller
+public class TiendaController {
+
+	@Autowired
+	private IMarcaRepository marca;
+	
+	@Autowired
+	private IProductoRepository produc;
+	
+	@GetMapping("/tienda")
+	public String listadoProducto(Model model) {
+		model.addAttribute("marca",new Marca());
+		model.addAttribute("lstMarcaporEstado",marca.findByEstado(1));
+		model.addAttribute("lstProductoporEstado",produc.findByEstado(1));
+
+		return "Tienda";
+	}
+	
+	
+	/*@RequestMapping("/detalle/{idproducto}")
+	public String verDetalle(@ModelAttribute Producto producto, Model model) {
+		model.addAttribute("produc",producto);
+		Producto prod = produc.findByidproducto(producto.getIdproducto());
+		return "DetalleProducto";
+	}*/
+	
+	@RequestMapping("/detalle/{idproducto}")
+	public String verDetalle(@PathVariable Integer idproducto, Model model) {
+	    // Obtener el producto desde la base de datos por su ID
+	    Producto producto = produc.findByidproducto(idproducto);
+
+	    if (producto != null) {
+	        model.addAttribute("producto", producto); // Usa "producto" en lugar de "produc"
+	    } else {
+	        // Manejar el caso en que el producto no se encuentra (por ejemplo, mostrar un mensaje de error o redirigir a otra página)
+	    }
+
+	    return "DetalleProducto";
+	}
+
+}
--- src/main/java/com/app/repository/IProductoRepository.java
+++ src/main/java/com/app/repository/IProductoRepository.java
@@ -1,13 +1,16 @@
 package com.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.app.modelos.Marca;
 import com.app.modelos.Producto;
 
 public interface IProductoRepository extends JpaRepository<Producto, Integer> {
 
 	Producto findByidproducto(Integer idproducto);
 	
+	List<Producto> findByEstado(int estado);
+
 	
 }
